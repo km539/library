a8c48e02f200f9942819ca05da8009e9f9620d26
kyojiMatsushima <kyoji.matsushima@asterone.co.jp>
2024-06-28 23:33:16 +0900

    creating modal

--- src/components/BooksList.tsx
+++ src/components/BooksList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import booksjson from "../books.json";
+import "../styles/BookList.css";
 
 type bookType = {
   id: string;
@@ -20,14 +21,52 @@ function BooksList() {
     getBooks().then(data => setBooks(data));
   }, []);
 
+  const viewDetail = (book: bookType) => (
+    <div className="overlay">
+      <div className="content">
+        <h2>{book.title}</h2>
+        <p>
+          <strong>Author:</strong> {book.author}
+        </p>
+        <p>
+          <strong>Genre:</strong> {book.genre}
+        </p>
+        <p>
+          <strong>Description:</strong> {book.description}
+        </p>
+        <p>
+          <strong>Published Date:</strong> {book.publishedDate}
+        </p>
+        <button>Close</button>
+      </div>
+    </div>
+  );
+
   return (
     <div>
-      <h1>Catalog</h1>
-      <div>
+      <h1>Book List</h1>
+      <table className="books">
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Author</th>
+          {/* <th>Gerne</th>
+          <th>Description</th>
+          <th>publishedDate</th> */}
+          <th>STATUS</th>
+        </tr>
         {books.map(book => (
-          <p>{book.title}</p>
+          <tr key={book.id} onClick={() => viewDetail(book)}>
+            <td>{book.id}</td>
+            <td>{book.title}</td>
+            <td>{book.author}</td>
+            {/* <td>{book.genre}</td>
+            <td>{book.description}</td>
+            <td>{book.publishedDate}</td> */}
+            <td>available</td>
+          </tr>
         ))}
-      </div>
+      </table>
     </div>
   );
 }
