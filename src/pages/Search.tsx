755ebd90c8e2850e7bf5efe1b59006dd0067291f
kyojiMatsushima <kyoji.matsushima@asterone.co.jp>
2024-07-05 08:03:21 +0900

    検索ページ作成

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 function Search() {
-  return <div>Search</div>;
+  const submitAction = e => {
+    e.preventDefault();
+    try {
+      const title = e.target.title.value;
+      const author = e.target.author.value;
+      //   console.log(name, password);
+    } catch (error) {
+      console.error("submitAction Error : ", error.message);
+    }
+  };
+
+  return (
+    <>
+      <h2 className="title">検索フォーム</h2>
+      <div>
+        <form action="" onSubmit={submitAction}>
+          <div className="container">
+            <label htmlFor="title">タイトル</label>
+            <input type="text" name="title" />
+            <label htmlFor="author">著者</label>
+            <input type="text" name="author" />
+            <label htmlFor="publishedDate">発行</label>
+            <input type="text" name="publishedDate" />
+            <button>検索</button>
+            <button>クリア</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Search;
